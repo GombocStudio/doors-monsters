b615c9baee3295e5962c5262cd59042d0a616b91
César Moltó Morilla <cmoltomorilla@gmail.com>
2022-02-25 16:12:41 +0100

    Added door prefab and fixed character movement bug

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,31 +7,39 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     // Reference to photonview component of the character. Prevents input from local player to influence every character in the scene.
-    PhotonView view;
+    PhotonView _view;
 
     // Character movement variables
-    private float speed = 10.0f;
-    private Vector3 movement = new Vector3(0, 0, 0);
+    private float _speed = 6.0f;
+    private Vector3 _movement = new Vector3(0, 0, 0);
+
+    private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     private void Start()
     {
         // Initialize photon view reference
-        view = GetComponent<PhotonView>();
+        _view = GetComponent<PhotonView>();
+
+        // Initialize rigid body component reference
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Method triggered when any of the characterActions.InputActions specified movement keys is pressed
     public void Move(InputAction.CallbackContext context)
     {
         // Updates movment vector if current view is from the local player
-        if (view && view.IsMine)
-            movement = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        if (_view && _view.IsMine)
+            _movement = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
 
     public void FixedUpdate()
     {
+        // Check if character has a rigidbody component assign to it
+        if (!_rigidbody) { return; }
+
         // Updates transform position of character if current view is from the local player
-        if (view && view.IsMine)
-            transform.position += movement * speed * Time.deltaTime;
+        if (_view && _view.IsMine)
+            _rigidbody.velocity = _movement * _speed;
     }
 }
