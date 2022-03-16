02f59946a5a589720f3ef983fa487fa63d97ad35
RaanGi <45555635+RaanGi@users.noreply.github.com>
2022-03-16 20:34:42 +0100

    Added final characters to the game

--- Assets/Scripts/Mechanics/MyCharacterController.cs
+++ Assets/Scripts/Mechanics/MyCharacterController.cs
@@ -7,9 +7,12 @@ using UnityEngine.InputSystem;
 public class MyCharacterController : MonoBehaviour
 {
     // Character movement related variables
-    private float _speed = 6.0f;
+    public float _speed = 1.0f;
     private float _rotSpeed = 5f;
-    private Vector3 _movement = new Vector3(0, 0, 0);
+    //private Vector3 _movement = new Vector3(0, 0, 0);
+    private Vector2 _movement;
+    private float _rotationVelocity;
+    private float _targetRotation;
 
     private Vector3 _networkPosition;
     private Quaternion _networkRotation;
@@ -23,11 +26,18 @@ public class MyCharacterController : MonoBehaviour
     // Door control time in seconds
     public float doorControlTime;
 
+    [Tooltip("How fast the character turns to face movement direction")]
+    [Range(0.0f, 0.3f)]
+    public float RotationSmoothTime = 0.12f;
+
     // Reference to character's rigidbody
     private Rigidbody _rigidbody;
 
     // Reference to photonview component of the character. Prevents input from local player to influence every character in the scene.
     private PhotonView _view;
+
+    //
+    private Animator _anim;
 
     // Start is called before the first frame update
     private void Start()
@@ -37,6 +47,9 @@ public class MyCharacterController : MonoBehaviour
 
         // Initialize rigid body component reference
         _rigidbody = GetComponent<Rigidbody>();
+
+        //
+        _anim = GetComponent<Animator>();
     }
 
     // Method triggered when any of the characterActions.InputActions specified movement keys is pressed
@@ -44,23 +57,40 @@ public class MyCharacterController : MonoBehaviour
     {
         // Updates movment vector if current view is from the local player
         if (_view && _view.IsMine)
-            _movement = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+            //_movement = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+            _movement = context.ReadValue<Vector2>();
+    }
+
+    //
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        _anim.SetBool("isAttacking", context.ReadValueAsButton());
+    }
+
+    private void Update()
+    {
+        _anim.SetBool("isWalking", (_movement.x != 0 || _movement.y != 0));
     }
 
     public void FixedUpdate()
     {
-        // Check if character has a rigidbody component assign to it
-        /* if (!_rigidbody) { return; }
-
-        // Updates transform position of character if current view is from the local player
-        if (_view && _view.IsMine)
-            _rigidbody.velocity = _movement * _speed; */
-
         Transform playerTransform = transform;
         if (_view && _view.IsMine)
         {
-            playerTransform.Rotate(Vector3.up * _movement.x * _rotSpeed);
-            playerTransform.Translate(Vector3.forward * _movement.y * _speed);
+            //playerTransform.Rotate(Vector3.up * _movement.x * _rotSpeed);
+            //playerTransform.Translate(Vector3.forward * _movement.y * _speed);
+
+            float speed = 0.0f;
+            if (_movement != Vector2.zero)
+            {
+                _targetRotation = Mathf.Atan2(_movement.x, _movement.y) * Mathf.Rad2Deg;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
+
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                speed = _speed;
+            }
+
+            transform.Translate(Vector3.forward * (speed * Time.deltaTime));
         }
         else
         {
