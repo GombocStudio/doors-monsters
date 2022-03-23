4489fd5fa1eef9f9cc5e6181a4fb443a31b3f2de
Roberto Perez <robj.perez@gmail.com>
2022-03-23 19:16:44 +0100

    Spawn monster in game scene

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,19 @@ using UnityEngine.AI;
 public class MonsterController : MonoBehaviour
 {
     public GameObject monsterPrefab;
-    public GameObject gameManager;
-
-    private TerrainGenerator mapGenerator;
-    private System.DateTime lastSpawnedTime = System.DateTime.Now;
-    
+    public GameObject terrainGenerator;
     public bool paused = false;
     public int maxMonsters = 50;
+
+    private TerrainGenerator mapGenerator;
+    private System.DateTime lastSpawnedTime = System.DateTime.Now;        
     private int activeMonsters = 0;
+    private bool usePhoton = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        mapGenerator = gameManager.GetComponent<TerrainGenerator>();        
-        //SpawnMonster();
+        mapGenerator = terrainGenerator.GetComponent<TerrainGenerator>();        
     }
 
     private TerrainStructure GetRandomRoom()
@@ -47,7 +47,14 @@ public class MonsterController : MonoBehaviour
         spawnPos.x += Random.Range(-1f, 1f);        
         spawnPos.z += Random.Range(-1f, 1f);
         spawnPos.y = 8.0f;
-        var monster = Instantiate(monsterPrefab, spawnPos, Quaternion.AngleAxis(0, Vector3.right));
+        GameObject monster;
+        if (usePhoton)
+        {
+            monster = PhotonNetwork.Instantiate(monsterPrefab.name, spawnPos, Quaternion.AngleAxis(0, Vector3.right));
+        } else
+        {
+            monster = Instantiate(monsterPrefab, spawnPos, Quaternion.AngleAxis(0, Vector3.right));
+        }
         monster.GetComponent<MonsterScript>().mapGenerator = mapGenerator;
     }
 
@@ -55,7 +62,9 @@ public class MonsterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (paused || activeMonsters >= maxMonsters) return;
+        if (paused 
+            || activeMonsters >= maxMonsters
+            || !PhotonNetwork.IsMasterClient) return;
 
         if (System.DateTime.Now - lastSpawnedTime > System.TimeSpan.FromMilliseconds(1000))
         {
